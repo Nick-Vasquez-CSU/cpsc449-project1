b7153c1ee07b2fc0acfcecb4d45c0e7adc4a0361
Sarthak-Gajjar <sarthakgajjar@csu.fullerton.edu>
2022-10-21 13:58:06 -0700

    logic issue - db update

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Group Number 10
 
 Team Members:
 
-Sarthak Gajjar - Wordle Database and Json data_pop script
+# Sarthak Gajjar - Wordle Database, Json data_pop and game data related issues
 
-Prashams Omprakash Daulath - Wordle Login Api
+# Prashams Omprakash Daulath - Wordle Login Api and user auth
 
-Steven Rico - Wordle Game Apis
+# Steven Rico - Wordle Game Apis and game logic
 
-Nicolas Vasquez - Wordle Game Apis
+# Nicolas Vasquez - Wordle Game Apis and game logic
 
 For initializing the new fresh database
 -run init.sh using "sh init.sh" command!
--- share/wordle.sql
+++ share/wordle.sql
@@ -15,18 +15,18 @@ CREATE TABLE user(
 CREATE TABLE id(
     game_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id VARCHAR(50),
-    play_id INTEGER,
     secret_word_id INTEGER,
     FOREIGN KEY(user_id) REFERENCES user(user_id),
-    FOREIGN KEY (play_id) REFERENCES play(play_id),
     FOREIGN KEY (secret_word_id) REFERENCES secret(secret_word_id)
 );	    
 
 CREATE TABLE play(
     play_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER,
     guessed_word VARCHAR(5),
     cr_lt_cr_pl VARCHAR(10),
     cr_lt_wr_pl VARCHAR(10)
+    FOREIGN KEY (game_id) REFERENCES id(game_id),
 );
 
 CREATE TABLE secret(
